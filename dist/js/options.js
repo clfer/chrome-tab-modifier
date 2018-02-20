f717eda74c9df04973bdec4511429b195b4eba59
clfer <clement.ferrier+github@gmail.com>
2018-02-20 02:57:35 +0100

    Fix notice caused by potentially empty items in chrome.storage.sync.get() callback

--- dist/js/options.js
+++ dist/js/options.js
@@ -207,7 +207,7 @@ app.controller('TabRulesController', ['$scope', '$routeParams', '$http', '$mdDia
     
     // Avoid BC break
     chrome.storage.sync.get('tab_modifier', function (items) {
-        if (items.tab_modifier !== undefined && items.tab_modifier !== null) {
+        if (items !== undefined && items.tab_modifier !== undefined && items.tab_modifier !== null) {
             tab_modifier.build(items.tab_modifier);
             tab_modifier.sync();
         }
@@ -223,7 +223,7 @@ app.controller('TabRulesController', ['$scope', '$routeParams', '$http', '$mdDia
     };
     
     chrome.storage.local.get('tab_modifier', function (items) {
-        if (items.tab_modifier !== undefined) {
+        if (items !== undefined && items.tab_modifier !== undefined) {
             tab_modifier.build(items.tab_modifier);
         }
         
